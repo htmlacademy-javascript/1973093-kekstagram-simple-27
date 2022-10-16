bdc6190a4d1c8a611f6729fbf36110faad3d16c6
Антон <a.a.pavlenko89@mail.ru>
2022-10-16 18:27:21 +0300

    Добавляет ДЗ 4-1-2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const ID = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
-const URL = ['photos/{{1}}.jpg','photos/{{2}}.jpg','photos/{{3}}.jpg','photos/{{4}}.jpg','photos/{{5}}.jpg','photos/{{6}}.jpg',
-  'photos/{{7}}.jpg','photos/{{8}}.jpg','photos/{{9}}.jpg','photos/{{10}}.jpg','photos/{{11}}.jpg','photos/{{12}}.jpg','photos/{{13}}.jpg',
-  'photos/{{14}}.jpg','photos/{{15}}.jpg','photos/{{16}}.jpg','photos/{{17}}.jpg','photos/{{18}}.jpg','photos/{{19}}.jpg','photos/{{20}}.jpg',
-  'photos/{{21}}.jpg','photos/{{22}}.jpg','photos/{{23}}.jpg','photos/{{24}}.jpg','photos/{{25}}.jpg'];
+const URL = ['photos/1.jpg','photos/2.jpg','photos/3.jpg','photos/4.jpg','photos/5.jpg','photos/6.jpg',
+  'photos/7.jpg','photos/8.jpg','photos/9.jpg','photos/10.jpg','photos/11.jpg','photos/12.jpg','photos/13.jpg',
+  'photos/14.jpg','photos/15.jpg','photos/16.jpg','photos/17.jpg','photos/18.jpg','photos/19.jpg','photos/20.jpg',
+  'photos/21.jpg','photos/22.jpg','photos/23.jpg','photos/24.jpg','photos/25.jpg'];
 
 const DESCRIPTION = 'Самая популярная фотография';
 
@@ -51,6 +51,6 @@ const descriptionPhoto = () => {
   };
 };
 
-const differentObject = Array.from({length: 25}, descriptionPhoto);
+const differentObject = Array.from({length: DIFFERENT_OBJECT}, descriptionPhoto);
 
 console.log(differentObject);
