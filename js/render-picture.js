e3fc9e64d6c28f4449f0f79de28d8624d481a58e
Антон <a.a.pavlenko89@mail.ru>
2022-11-21 22:10:45 +0300

    Защита №2-1

--- js/render-picture.js
+++ js/render-picture.js
@@ -5,11 +5,11 @@ const randomUsersImageTemplate = document.querySelector('#picture').content.quer
 const picturesBlock = document.querySelector('.pictures');
 
 const renderPicture = (url, likes, comments) => {
-    const pictureElement = randomUsersImageTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments;
-    picturesBlock.append(pictureElement);
+  const pictureElement = randomUsersImageTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  picturesBlock.append(pictureElement);
 };
 
 const renderPictures = (data) => {
